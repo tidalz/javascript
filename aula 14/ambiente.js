93a1dff1d83058eaf6638e599575d49646a7d8c9
tidalz <pedrowknabben@gmail.com>
2024-11-25 16:21:46 -0300

    Create ambiente.js

---
+++ aula 14/ambiente.js
@@ -0,0 +1,16 @@
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+ */
+
+// Mesma coisa do while escrito acima
+
+console.log('Vai começar...')
+for(var c =1;c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
